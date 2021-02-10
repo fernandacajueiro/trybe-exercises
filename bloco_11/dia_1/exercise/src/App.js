7f1c251c4a34aae1c9677b322aa1fcc08edf4948
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-02-10 19:33:25 -0300

    Adds exercises day 1 block 11

---
+++ bloco_11/dia_1/exercise/src/App.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import './App.css';
+
+const array = ['JavaScript', 'React', 'Jest'];
+
+const task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+class Tarefas extends Component {
+  render() {
+    const elemento = array.map(tarefa => task(tarefa));
+    return (
+      <div className="lista-tarefas">
+        {elemento}
+      </div>
+    );
+  }
+}
+
+export default Tarefas;
